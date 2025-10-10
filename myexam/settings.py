bdce2b285a672f23578c672c28348a5bbb13262e
Anton Chuvashov <chuvashov_a@icloud.com>
2025-10-10 16:19:59 +0500

    add trusted origins

--- myexam/settings.py
+++ myexam/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 
 
 # Application definition
