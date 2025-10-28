126343b4adabb0154b5cc45195f5d5ae0273b875
Anton Chuvashov <chuvashov_a@icloud.com>
2025-10-28 11:32:11 +0500

    add more cards

--- tests/templatetags/dashboard_tags.py
+++ tests/templatetags/dashboard_tags.py
@@ -1,6 +1,7 @@
 from django import template
-from django.db.models import Count, Avg
-from ..models import UserTestSession, Test, Grade
+from django.db.models import Count, Avg, F
+from django.contrib.auth.models import Group
+from ..models import UserTestSession, Test, Grade, User
 
 register = template.Library()
 
@@ -24,3 +25,27 @@ def average_score_chart():
         'labels': [t.title for t in tests],
         'data': [round(t.avg_score, 2) for t in tests],
     }
+
+@register.inclusion_tag('admin/dashboard/partials/group_average_scores.html')
+def group_average_scores():
+    groups = Group.objects.annotate(avg_score=Avg('user__usertestsession__score_percent')).filter(avg_score__isnull=False)
+    return {
+        'labels': [g.name for g in groups],
+        'data': [round(g.avg_score, 2) for g in groups],
+    }
+
+@register.inclusion_tag('admin/dashboard/partials/test_performance.html')
+def test_performance_summary():
+    tests = Test.objects.annotate(avg_score=Avg('usertestsession__score_percent')).filter(avg_score__isnull=False).order_by('-avg_score')
+    return {
+        'best_tests': tests[:5],
+        'worst_tests': tests.reverse()[:5],
+    }
+
+@register.inclusion_tag('admin/dashboard/partials/top_students.html')
+def top_students(count=10):
+    students = User.objects.annotate(
+        avg_score=Avg('usertestsession__score_percent'),
+        test_count=Count('usertestsession')
+    ).filter(avg_score__isnull=False).order_by('-avg_score')[:count]
+    return {'students': students}
